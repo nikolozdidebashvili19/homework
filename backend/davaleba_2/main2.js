00bb7830ca887e9dfe9866cd19f95f9be564e12e
ERRORniku404 <nikuu4879@gmail.com>
2026-03-08 11:20:58 +0400

    commit message

--- backend/davaleba_2/main2.js
+++ backend/davaleba_2/main2.js
@@ -1,13 +1,19 @@
-// import fs from "fs/promises";
-// import axios from "axios";
+import fs from "fs/promises";
+import axios from "axios";
 
+async function main() {
+  const response = await axios.get("https://dummyjson.com/users");
+  const users = response.data.users;
+  const extractedData = users.map((user) => {
+    return {
+      id: user.id,
+      fullName: `${user.firstName} ${user.lastName}`,
+      email: user.email,
+      birthDate: user.birthDate,
+      country: user.address?.country,
+    };
+  });
+  await fs.writeFile("users.json", JSON.stringify(extractedData));
+}
 
-// async function main() {
-//     const link = await axios.get("https://dummyjson.com/users");
-//      const users = link.data.users;
-//      const parsed = JSON.parse(users)
-//      console.log(parsed)
-// }
-
-// main();
-
+main();
