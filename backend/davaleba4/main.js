3ec1424496f9ef3a82bf5d5c7d0f7748c01b2a84
ERRORniku404 <nikuu4879@gmail.com>
2026-04-16 16:57:27 +0400

    cvlilebebi

--- backend/davaleba4/main.js
+++ backend/davaleba4/main.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app = express();
 const fs = require("fs/promises");
 const animalsRouter = require("./animals.router");
+const blogsRouter = require("./blogs.router");
 app.use(express.json());
 
 
 
 app.use("/animals" , animalsRouter  )
+app.use("/blogs" , blogsRouter )
 
 app.listen(3000, () => {
   console.log("server running on https://localhost:3000");
